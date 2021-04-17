9946f7d9842ae33784fd3755ebafd2404c26b363
YEA <yared@DESKTOP-ABC93QD.localdomain>
2021-04-17 20:25:52 +0200

    reshuffeled data stuf

--- data/createData.py
+++ data/createData.py
@@ -3,26 +3,26 @@ import random, csv, uuid, lorem
 # Variables
 companies = ['Spotify', 'Klarna', 'King', 'Mojang', 'Unibet', 'Google', 'Facebook', 'Apple', 'Netflix', 'iZettle']
 #fompanies = ['Scotty-Phi', 'Kvarna', 'Queen', 'Mogang', 'DuoBet', 'Foogle', 'BaseBook', 'Pear', 'WebFlicks', 'uPay'] #Fake companies
-job_titles = ['Front-End Developer', 'Back-End Developer', 'Mobile Developer', 'QA Developer', 'DevOps Engineer', 'Cloud Engineer', 'HR Officer', 'UX Designer',]
 
+job_titles = ['Front-End Developer', 'Back-End Developer', 'Mobile Developer', 'QA Developer', 'DevOps Engineer', 'Cloud Engineer', 'HR Officer', 'UX Designer']
 seniorities_matrix = [['Intern', 1], ['Junior', 3], ['Mid-level', 6], ['Senior', 4], ['Lead', 2], ['Principal', 1]]
 # Different levels of seniorities + their weight as a probability
 seniorities = [row[0] for row in seniorities_matrix]
 seniorities_weights = [row[1] for row in seniorities_matrix]  
 
-fnames = ['reviewId', 'reviewEditCode', 'company', 'seniority', 'jobTitle', 'reviewBody', 'reaction1', 'reaction2', 'reaction3', 'reaction4']
+fnames = ['company', 'reviewId', 'reviewEditCode', 'jobDescription', 'reviewBody', 'reaction1', 'reaction2', 'reaction3', 'reaction4']
 
 def getFakeReview():
-  return {fnames[0] : str(uuid.uuid4()),
-          fnames[1] : str(uuid.uuid4()),
-          fnames[2] : random.choice(companies), 
-          fnames[3] : random.choices(seniorities, weights=seniorities_weights, k=1)[0],
-          fnames[4] : random.choice(job_titles),
-          fnames[5] : lorem.get_paragraph(count=random.randint(1,3)),
-          fnames[6] : max(int(random.gauss(1, 7)), 0),
-          fnames[7] : max(int(random.gauss(1, 3)), 0),
-          fnames[8] : max(int(random.gauss(1, 2)), 0),
-          fnames[9] : max(int(random.gauss(1, 1)), 0)
+  return {
+          fnames[0] : random.choice(companies), 
+          fnames[1] : str(uuid.uuid4())[:8],
+          fnames[2] : str(uuid.uuid4())[:8],
+          fnames[3] : random.choices(seniorities, weights=seniorities_weights, k=1)[0] + ' ' + random.choice(job_titles),
+          fnames[4] : lorem.get_paragraph(count=random.randint(1,3)),
+          fnames[5] : max(int(random.gauss(1, 7)), 0),
+          fnames[6] : max(int(random.gauss(1, 3)), 0),
+          fnames[7] : max(int(random.gauss(1, 2)), 0),
+          fnames[8] : max(int(random.gauss(1, 1)), 0)
           }
 
 
---
+++ data/uploadData.py
@@ -0,0 +1,35 @@
+import sys, csv, boto3
+
+"""
+Uploads data to DynamoDB
+"""
+
+if len(sys.argv) < 2:
+  raise Exception('Missing TableName Argument!')
+table_name = str(sys.argv[1])
+
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(table_name)
+try:
+  str(table.creation_date_time)
+except Exception as e:
+  print(e)
+
+with open('data.csv', 'r') as f:
+  reader = csv.reader(f, delimiter=';')
+  headers = reader.__next__()
+  with table.batch_writer() as dbwriter:
+    for row in reader:
+      dbwriter.put_item(
+        Item={
+          headers[0] : row[0],
+          headers[1] : row[1],
+          headers[2] : row[2],
+          headers[3] : row[3],
+          headers[4] : row[4],
+          headers[5] : row[5],
+          headers[6] : row[6],
+          headers[7] : row[7],
+          headers[8] : row[8],
+        })
