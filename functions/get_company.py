8a99789935aebb0a0b24920a02a54697cbc3389f
YEA <yared@DESKTOP-ABC93QD.localdomain>
2021-04-17 22:42:07 +0200

    gets all reviews by company

--- functions/get_company.py
+++ functions/get_company.py
@@ -1,24 +1,20 @@
-import json
+import boto3, os, json
+from boto3.dynamodb.conditions import Key
 
+table = boto3.resource('dynamodb').Table(os.environ['DDBTableName'])
 
 def handler(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    company = event['queryStringParameters']['q']
 
+
+
+    dbResponse = table.query(
+            KeyConditionExpression=Key('company').eq(company)
+    )
+            
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(dbResponse)
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
